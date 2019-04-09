d80936faeb238dacdc4b4d290935db52807e1463
mwiski <wiskim1@gmail.com>
2019-04-09 21:23:41 +0200

    KOD-6.2: added classes Calculator and SimpleUser, and tested it

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,28 @@
 package com.kodilla.testing;
 
-import java.sql.SQLOutput;
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.add(3, 5));
+        System.out.println(calculator.subtract(18, 6));
     }
 }
+
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public double add(int a, int b) {
+        return a + b;
+    }
+
+    public double subtract(int a, int b) {
+        return a - b;
+    }
+}
