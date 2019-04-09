ae6626843cd242d492ea9a3848ad4d543049d4ca
mwiski <49036916+mwiski@users.noreply.github.com>
2019-04-09 20:50:17 +0200

    KOD-6.1: created new module kodilla-testing (#1)

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing;
+
+import java.sql.SQLOutput;
+
+public class TestingMain {
+    public static void main(String[] args) {
+        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    }
+}
