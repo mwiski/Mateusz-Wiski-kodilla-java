8bb3b9fc9acd0f73820aeee1f3e8cacd6fc64977
mwiski <49036916+mwiski@users.noreply.github.com>
2019-04-10 15:02:23 +0200

    KOD-6.2: added classes Calculator and SimpleUser, and tested it (#2)
    * KOD-6.2: added classes Calculator and SimpleUser, and tested it
    
    * KOD-6.2: changed test
    
    * KOD-6.2: divided test into two tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,40 @@
 package com.kodilla.testing;
 
-import java.sql.SQLOutput;
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        int addTest = calculator.add(3, 5);
+        int subtractTest = calculator.subtract(18, 6);
+
+        if (addTest == 8) {
+            System.out.println("Success! Calculator's method add() test was positive!");
+        } else {
+            System.out.println("Error! Calculator's method add() is not working properly.");
+        }
+
+        if (subtractTest == 12) {
+            System.out.println("Success! Calculator's method subtract test was positive!");
+        } else {
+            System.out.println("Error! Calculator's method subtract() is not working properly.");
+        }
     }
 }
+
