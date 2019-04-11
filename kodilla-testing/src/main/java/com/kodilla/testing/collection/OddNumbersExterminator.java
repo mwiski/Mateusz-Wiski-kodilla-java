4b52c44b32e8102909a0c667168f3fab098c228c
mwiski <wiskim1@gmail.com>
2019-04-11 16:12:36 +0200

    KOD-6.3: changed Test Suite

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -4,32 +4,13 @@ import java.util.*;
 
 public class OddNumbersExterminator {
 
-    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
+    public List<Integer> exterminate(ArrayList<Integer> numbers) {
 
         ArrayList<Integer> numbersEven = new ArrayList<>();
         for (Integer number : numbers) {
-            if (number == 0 || number % 2 == 0)
+            if (number % 2 == 0)
                 numbersEven.add(number);
         }
-        numbers.clear();
-        numbers.addAll(numbersEven);
-        return numbers;
-    }
-
-    private static ArrayList<Integer> initList(ArrayList<Integer> numbers) {
-        numbers.add(0);
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(6);
-        numbers.add(7);
-
-        return numbers;
-    }
-
-    public ArrayList<Integer> init(ArrayList<Integer> numbers) {
-        return initList(numbers);
+        return numbersEven;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,53 +1,71 @@
 package com.kodilla.testing.collection;
 import com.kodilla.testing.collection.OddNumbersExterminator;
 import org.junit.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionTestSuite {
+
     @Before
     public void before(){
         System.out.println("Test Case: begin");
     }
+
     @After
     public void after(){
         System.out.println("Test Case: end");
     }
+
     @BeforeClass
     public static void beforeClass() {
         System.out.println("Test Suite: begin");
     }
+
     @AfterClass
     public static void afterClass() {
         System.out.println("Test Suite: end");
     }
+
     @Test
     public void testOddNumbersExterminatorEmptyList() {
         //Given
         OddNumbersExterminator exterminator = new OddNumbersExterminator();
         ArrayList<Integer> numbers = new ArrayList<>();
+
         //When
-        ArrayList<Integer> numbers2 = exterminator.exterminate(numbers);
+        List<Integer> result = exterminator.exterminate(numbers);
         System.out.println("Testing empty list case");
+
         //Then
-        Assert.assertArrayEquals(numbers.toArray(), numbers2.toArray());
+        Assert.assertEquals(numbers, result);
     }
+
     @Test
     public void testOddNumbersExterminatorNormalList () {
         //Given
         OddNumbersExterminator exterminator = new OddNumbersExterminator();
+
         ArrayList<Integer> numbers = new ArrayList<>();
-        exterminator.init(numbers);
-        ArrayList<Integer> result = new ArrayList<>();
-        result.add(0);
-        result.add(2);
-        result.add(4);
-        result.add(6);
+        numbers.add(0);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        expected.add(2);
+        expected.add(4);
+        expected.add(6);
+
         //When
-        exterminator.exterminate(numbers);
+        List<Integer> result = exterminator.exterminate(numbers);
         System.out.println("Testing normal list case");
+
         //Then
-        Assert.assertArrayEquals(result.toArray(), numbers.toArray());
+        Assert.assertEquals(expected, result);
     }
 }
