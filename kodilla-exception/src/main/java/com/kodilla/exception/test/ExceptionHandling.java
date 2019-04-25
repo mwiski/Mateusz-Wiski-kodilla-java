2a6037cf27d3cb26e34ef897f5ed81556a83860f
mwiski <wiskim1@gmail.com>
2019-04-25 21:45:41 +0200

    KOD-8.2: ExceptionHandling

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(2, 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
