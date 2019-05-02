63f15eacbe98bfb849d477e1a3b41d60f50860bb
mwiski <49036916+mwiski@users.noreply.github.com>
2019-05-02 12:04:45 +0200

    KOD-8.3: RouteNotFoundException (#14)
    * KOD-8.3: RouteNotFoundException
    
    * KOD-8.3: changed method findFlight
    
    * KOD-8.3: deletef final markers
    
    * KOD-8.3:

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightsProcessor.java
@@ -0,0 +1,20 @@
+package com.kodilla.exception.test;
+
+import java.util.Map;
+
+public class FlightsProcessor {
+    private Map<String, Boolean> airports;
+
+    public FlightsProcessor(Map<String, Boolean> airports) {
+        this.airports = airports;
+    }
+
+    public boolean findFlight(Flight flight) throws RouteNotFoundException {
+
+        try {
+            return airports.get(flight.getArrivalAirport());
+        } catch (NullPointerException e) {
+            throw new RouteNotFoundException("Route to " + flight.getArrivalAirport() + " not found");
+        }
+    }
+}
