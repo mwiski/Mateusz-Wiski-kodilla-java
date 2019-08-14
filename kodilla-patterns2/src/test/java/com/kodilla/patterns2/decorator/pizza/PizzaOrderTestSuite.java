0ac856d32472f0ad16414a3fa09067383ffa5c32
mwiski <wiskim1@gmail.com>
2019-08-14 23:34:57 +0200

    KOD-24.2: pizza decorator

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
@@ -0,0 +1,117 @@
+package com.kodilla.patterns2.decorator.pizza;
+
+import org.junit.Test;
+import java.math.BigDecimal;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class PizzaOrderTestSuite {
+    @Test
+    public void testBasicPizzaOrderGetCost() {
+        //Given
+        PizzaOrder pizzaOrder = new BasicPizzaOrder();
+
+        //When
+        BigDecimal totalCost = pizzaOrder.getCost();
+
+        //Then
+        assertThat(totalCost).isEqualTo(new BigDecimal(15));
+    }
+
+    @Test
+    public void testBasicPizzaOrderGetDescription() {
+        //Given
+        PizzaOrder pizzaOrder = new BasicPizzaOrder();
+
+        //When
+        String description = pizzaOrder.getDescription();
+
+        //Then
+        assertThat(description).isEqualTo("Pizza with tomato sauce and cheese");
+    }
+
+    @Test
+    public void testPizzaWithMushroomsTaxiNetworkGetCost() {
+        //Given
+        PizzaOrder pizzaOrder = new BasicPizzaOrder();
+        pizzaOrder = new MushroomDecorator(pizzaOrder);
+
+        //When
+        BigDecimal totalCost = pizzaOrder.getCost();
+
+        //Then
+        assertThat(totalCost).isEqualTo(new BigDecimal(18));
+    }
+
+    @Test
+    public void testPizzaWithMushroomsTaxiNetworkGetDescription() {
+        //Given
+        PizzaOrder pizzaOrder = new BasicPizzaOrder();
+        pizzaOrder = new MushroomDecorator(pizzaOrder);
+
+        //When
+        String description = pizzaOrder.getDescription();
+
+        //Then
+        assertThat(description).isEqualTo("Pizza with tomato sauce and cheese + mushrooms");
+    }
+
+    @Test
+    public void testPizzaWithManyIngredientsGetCost() {
+        //Given
+        PizzaOrder pizzaOrder = new BasicPizzaOrder();
+        pizzaOrder = new MushroomDecorator(pizzaOrder);
+        pizzaOrder = new HamDecorator(pizzaOrder);
+        pizzaOrder = new HamDecorator(pizzaOrder);
+        pizzaOrder = new OnionDecorator(pizzaOrder);
+
+        //When
+        BigDecimal totalCost = pizzaOrder.getCost();
+
+        //Then
+        assertThat(totalCost).isEqualTo(new BigDecimal(30));
+    }
+
+    @Test
+    public void testPizzaWithManyIngredientsGetDescription() {
+        //Given
+        PizzaOrder pizzaOrder = new BasicPizzaOrder();
+        pizzaOrder = new MushroomDecorator(pizzaOrder);
+        pizzaOrder = new HamDecorator(pizzaOrder);
+        pizzaOrder = new HamDecorator(pizzaOrder);
+        pizzaOrder = new OnionDecorator(pizzaOrder);
+
+        //When
+        String description = pizzaOrder.getDescription();
+
+        //Then
+        assertThat(description).isEqualTo("Pizza with tomato sauce and cheese + mushrooms + ham + ham + onion");
+    }
+
+    @Test
+    public void testPizzaWithHamAndDeliveryGetCost() {
+        //Given
+        PizzaOrder pizzaOrder = new BasicPizzaOrder();
+        pizzaOrder = new HamDecorator(pizzaOrder);
+        pizzaOrder = new DeliveryDecorator(pizzaOrder);
+
+        //When
+        BigDecimal totalCost = pizzaOrder.getCost();
+
+        //Then
+        assertThat(totalCost).isEqualTo(new BigDecimal(30));
+    }
+
+    @Test
+    public void testPizzaWithHamAndDeliveryGetDescription() {
+        //Given
+        PizzaOrder pizzaOrder = new BasicPizzaOrder();
+        pizzaOrder = new HamDecorator(pizzaOrder);
+        pizzaOrder = new DeliveryDecorator(pizzaOrder);
+
+        //When
+        String description = pizzaOrder.getDescription();
+
+        //Then
+        assertThat(description).isEqualTo("Pizza with tomato sauce and cheese + ham with delivery to client");
+    }
+}
