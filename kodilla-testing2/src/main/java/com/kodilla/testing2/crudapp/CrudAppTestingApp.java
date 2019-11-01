c91f47a484227d057c4a740ebc420291194e129c
mwiski <wiskim1@gmail.com>
2019-11-01 12:49:57 +0100

    KOD-31.3: XPath-Relative

--- kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
@@ -10,7 +10,8 @@ public class CrudAppTestingApp {
 
     public static final String XPATH_INPUT = "//html/body/main/section/form/fieldset/input";
     public static final String XPATH_TEXTAREA = "//html/body/main/section/form/fieldset[2]/textarea";
-    public static final String XPATH_SELECT = "//select[1]";
+    public static final String XPATH_WAIT_FOR = "//select[1]";
+    public static final String XPATH_SELECT = "//div[contains(@class, \"tasks-container\")]/form/div/fieldset/select[1]";
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
@@ -22,7 +23,7 @@ public class CrudAppTestingApp {
         WebElement textareaField = driver.findElement(By.xpath(XPATH_TEXTAREA));
         textareaField.sendKeys("The robotic content");
 
-        while (!driver.findElement(By.xpath(XPATH_SELECT)).isDisplayed());
+        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR)).isDisplayed());
 
         WebElement selectCombo = driver.findElement(By.xpath(XPATH_SELECT));
         Select selectBoard = new Select(selectCombo);
